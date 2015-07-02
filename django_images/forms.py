2a4202d1ee29ab252780b6db8a67ce99367e8a72
Amirouche Boubekki <amirouche@vingtcinq.io>
2015-07-02 10:40:36 +0200

    forms: move code to PictureForm.is_valid, split validation and save

--- django_images/forms.py
+++ django_images/forms.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
 from django import forms
-from django.utils.text import slugify
+from cStringIO import StringIO
 
 from imageresize import imageresize
 
-from .settings import PICTURE_FORMATS
 from .models import PICTURE_CHOICES
-from .pictt import save
+from .settings import PICTURE_FORMATS
 
 
 class PictureForm(forms.Form):
 
-    picture = forms.FileField(label=u"Photo")
-    ptype = forms.ChoiceField(choices=PICTURE_CHOICES, label=u"Type de photo")
-    name = forms.CharField(label=u"Nom")
+    picture = forms.FileField()
+    ptype = forms.ChoiceField(
+        choices=PICTURE_CHOICES,
+        label=u"Type de photo"
+    )
+    name = forms.CharField()
 
-    def clean(self):
-        data = super(PictureForm, self).clean()
-        picture = data.get('picture')
-        ptype = data.get('ptype')
-
-        if picture and ptype:
-            formats = PICTURE_FORMATS[str(ptype)]
+    def is_valid(self):
+        if not super(PictureForm, self).is_valid():
+            return False
+        else:
+            data = self.clean()
+            # compute max constraint size with method
+            formats = PICTURE_FORMATS[str(data['ptype'])]
             maximum = formats['sizes'].values()[0]['size']
             method = formats['sizes'].values()[0]['method']
             for value in formats['sizes'].values()[1:]:
@@ -32,37 +34,23 @@ class PictureForm(forms.Form):
                     maximum = size
             method = 'resize_' + method
             method = getattr(imageresize, method)
+
+            # Create a clone of the picture to use with validation
+            # We need to do this because `Image.open` close the
+            # `picture` file which can not be opened later
+            picture = data['picture']
+            clone = StringIO()
+            clone.write(picture.file.read())
+
             try:
-                with Image.open(picture) as image:
+                # do validation against the max constraint
+                with Image.open(clone) as image:
                     method.validator(image, maximum)
             except Exception as exc:
-                error = forms.ValidationError(exc.message)
-                self.add_error('picture', error)
-
-
-class PictureTypeForm(forms.Form):
-
-    picture = forms.FileField(label=u"Photo")
-
-    def __init__(self, ptype, name, *args, **kwargs):
-        self.ptype = ptype
-        self.name = name
-        super(PictureTypeForm, self).__init__(*args, **kwargs)
-
-    def clean(self):
-        cleaned_data = super(PictureTypeForm, self).clean()
-        picture = cleaned_data.get('picture')
-        ptype = self.ptype
-        name = self.name
-        if picture and ptype and name:
-            filename = slugify(name)
-            try:
-                pict = save(picture, filename, int(ptype))
-            except Exception as e:  # FIXME: no generic exception
-                error = forms.ValidationError(e.message)
-                self.add_error('picture', error)
+                # the image doesn't satify the constraint.
+                self.add_error('picture', exc.message)
+                return False
             else:
-                self.pict = pict
-
-    def get_pict(self):
-        return self.pict
+                # It's ok,  reset picture file seek position for later re-use
+                picture.file.seek(0)
+                return True
