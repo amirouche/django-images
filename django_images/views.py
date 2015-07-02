fbb6d79e49c98eb8e1c7d4772eb3ecf4a175d970
Amirouche Boubekki <amirouche@vingtcinq.io>
2015-07-02 10:47:54 +0200

    views: split validation and save (second half)
    For now the image is resized and saved in the view like a regular model.

--- django_images/views.py
+++ django_images/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.utils.text import slugify
 from django.http import HttpResponseRedirect
 
-from .forms import PictureForm
+from .pictt import save
 from .models import Picture
+from .forms import PictureForm
 
 
 def debug(request):
@@ -11,6 +13,13 @@ def debug(request):
     if request.method == 'POST':
         form = PictureForm(request.POST, request.FILES)
         if form.is_valid():
+            data = form.cleaned_data
+            filename = slugify(data['name'])
+            save(
+                data['picture'],
+                filename,
+                data['ptype']
+            )
             return HttpResponseRedirect('/')
     pictures = Picture.objects.all()
     return render(request, 'django_images.html', {
