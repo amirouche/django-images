08bb9a0f83a09c3cb438f668d0ec85cc617244ac
Amirouche Boubekki <amirouche@vingtcinq.io>
2015-07-06 18:31:29 +0200

    admin: customize Image model admin list display

--- django_images/admin.py
+++ django_images/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image
+
+
+def size_display_factory(size):
+    def getter(model):
+        infos = getattr(model, size)
+        html = '<a href="%s">%s</a>'
+        label = '%sx%s' % (infos['width'], infos['height'])
+        html = html % (
+            model.url(size),
+            label
+        )
+        return html
+    getter.__name__ = size
+    getter.allow_tags = True
+    getter.short_description = size
+    return getter
+
+
+class ImageAdmin(admin.ModelAdmin):
+
+    list_display = ['preview', 'name', 'xs', 'sm', 'md', 'lg', 'og']
+
+    def __init__(self, *args, **kwargs):
+        for size in ['xs', 'sm', 'md', 'lg', 'og']:
+            setattr(self, size, size_display_factory(size))
+
+        super(ImageAdmin, self).__init__(*args, **kwargs)
+
+    def preview(self, model):
+        html = '<img src="%s" alt="%s"/>' % (
+            model.xs['url'],
+            model.name
+        )
+        return html
+    preview.allow_tags = True
+
+admin.register(Image)(ImageAdmin)
